f028dd0afe00f235678ece95b477cb16daced01e
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-02-04 14:20:04 +0100

    Add delete user function

--- script.js
+++ script.js
@@ -164,8 +164,6 @@ btnTransfer.addEventListener('click', function (e) {
   );
   inputTransferAmount.value = inputTransferTo.value = '';
 
-  console.log(currentAccount);
-  console.log(receiverAcc);
   if (
     amount > 0 &&
     receiverAcc &&
@@ -178,6 +176,27 @@ btnTransfer.addEventListener('click', function (e) {
     // Update UI
     updateUI(currentAccount);
   }
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
 });
 
 ///////////// TEST SECTION /////////////////
